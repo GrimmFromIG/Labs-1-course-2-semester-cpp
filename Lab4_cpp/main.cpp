de9580562890f9e16a3753ab8c3ebca7042944ea
GrimmFromIG <gamerboy200715@gmail.com>
2025-04-04 07:44:45 +0300

    c

---
+++ Lab4_cpp/main.cpp
@@ -0,0 +1,9 @@
+﻿#include "trapezoid.h"
+
+int main()
+{
+    std::vector<std::pair<double, double>> coords = {{0, 0}, {4, 0}, {3, 2}, {1, 2}};
+    Trapezoid trap(coords);
+    trap.printData();
+    return 0;
+}
